d64e619e4177397901ea1bc4319c19cf00916b9c
BALLNAZZAR-PC\Ballnazzar <ludek.p@email.cz>
2022-07-01 21:24:05 +0200

    Add wiremock test

---
+++ src/test/java/guru/springframework/brewery/events/BeerOrderStatusChangeEventListenerTest.java
@@ -0,0 +1,53 @@
+package guru.springframework.brewery.events;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import guru.springframework.brewery.domain.BeerOrder;
+import guru.springframework.brewery.domain.OrderStatusEnum;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+class BeerOrderStatusChangeEventListenerTest {
+
+    WireMockServer wireMockServer = new WireMockServer();
+
+    BeerOrderStatusChangeEventListener listener;
+
+    @BeforeEach
+    void setUp() {
+        wireMockServer.start();
+        //configureFor("localhost", 8080);
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        listener = new BeerOrderStatusChangeEventListener(restTemplateBuilder);
+    }
+
+    @AfterEach
+    void tearDown() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    void listen() {
+
+        wireMockServer.stubFor(post("/update").willReturn(ok()));
+
+        BeerOrder beerOrder = BeerOrder.builder()
+                .orderStatus(OrderStatusEnum.READY)
+                .orderStatusCallbackUrl("http://localhost:" + wireMockServer.port() + "/update")
+                .createdDate(Timestamp.valueOf(LocalDateTime.now()))
+                .build();
+
+        BeerOrderStatusChangeEvent event = new BeerOrderStatusChangeEvent(beerOrder, OrderStatusEnum.NEW);
+
+        listener.listen(event);
+
+        verify(1, postRequestedFor(urlEqualTo("/update")));
+
+    }
+}
